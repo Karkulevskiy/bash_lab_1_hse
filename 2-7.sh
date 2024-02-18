591cd6689511bae84d0c6d52a6de4f4dbdae3004
Max <119725856+Spacikl@users.noreply.github.com>
2024-02-18 14:03:51 +0300

    Update 2-7.sh

--- 2-7.sh
+++ 2-7.sh
@@ -1,31 +1,44 @@
 #!/bin/bash
 #This is a comment
 #Use local variables:
-#userName -> User name
 #first -> start index of sequence
 #second -> end index of sequence
 #help -> for comparing --help with variable
-userName=$1
-first=$2
-second=$3
-help="--help"
+first=$1
+second=$2
+regex='^[0-9]+$'
 #instructions for help
-if [[ $userName == $help ]];then
-	echo "INSTRUCTIONS"
-	echo "param1 (write user name), param2(first sequence), param3(second sequence)"
-	echo "Exit codes: Error(-1), Success(0)"
- 	echo "File users.csv will be saved with path: /home/users.csv"
-else	
+if [ "$first" = "--help" ];then
+	echo "INSTRUCTIONS:"
+	echo "STDIN: ./script <<< [user name]  OPTIONS(first sequence) OPTIONS(second sequence)"
+	echo "Exit codes: Error(-1), Success(0)" >&2
+	exit 0
+else
+	read userName
+	if [ -z "$userName" ];then
+		echo "User name can't be null or empty" >&2
+		exit -1
+	fi
 	#Handle error with exit code -1 if the arguments were wrong
+	if [ -z $first ] || [ -z $second ];then
+		echo "Sequence or user name can't be null" >&2
+		exit -1
+	fi
+	if ! [[ "$first" == ?(-)+([0-9]) ]] || ! [[ "$second" == ?(-)+([0-9]) ]];then
+		echo "Sequence must be a number" >&2
+		exit -1
+	fi
 	if [ $first -gt $second ] || [ $first -lt 0 ] || [ $second -lt 0 ];then
-		 echo "Error (-1): sequence number should be less or equal to second"
+		 echo "Error (-1): sequence number should be less or equal to second" >&2
+		 exit -1
 	else	#Write names of columns is csv file
-		echo "User name		       Sequence number			Password" >> $HOME/users.csv
+		echo "User name,Sequence number,Password" >> $HOME/users.csv
 		while [ $first -le $second ] #cycle with incrementing param index and writting in file
 		do #if file dosn't exist, he will be created
-			echo "$userName		$first			$(pwgen -s 20 -1)" >> $HOME/users.csv #append data
+			echo "$userName,$first,$(pwgen -s 20 -1)" >> $HOME/users.csv #append data
 			first=$[$first + 1]
 		done
-		echo "Success (0)" #return with exit code 0 -> success
+		echo "Success (0)" >&2 #return with exit code 0 -> success
+		exit 0
 	fi
 fi
